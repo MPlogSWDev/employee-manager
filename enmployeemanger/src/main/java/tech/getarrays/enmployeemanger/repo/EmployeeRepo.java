8479830dc6c6e279da167df5b9a49481b2eeb668
Happy <Mplogswdev@outlook.com>
2021-10-10 20:28:35 -0500

    EmployeeSevice class created

--- enmployeemanger/src/main/java/tech/getarrays/enmployeemanger/repo/EmployeeRepo.java
+++ enmployeemanger/src/main/java/tech/getarrays/enmployeemanger/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package tech.getarrays.enmployeemanger.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.getarrays.enmployeemanger.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
